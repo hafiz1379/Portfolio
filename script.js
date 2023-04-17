c26621798d84c40f84de2d6760bcd072ac497b29
hafiz1379 <hafizrasa1379@gmail.com>
2023-04-18 00:35:14 +0430

    Solved linters errors of script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('hashchange', () => {
-    if (window.location.hash === '#menu') {
-      document.body.classList.add('header--open-menu');
-    } else {
-      document.body.classList.remove('header--open-menu');
-    }
-  });
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('header--open-menu');
+  } else {
+    document.body.classList.remove('header--open-menu');
+  }
+});
